b10d61a48a816ca81d31f4ce7f69c01abb2265f7
Ehud Perlman <ehudperlman83@gmail.com>
2019-07-13 13:25:27 +0300

    added user list and preview

---
+++ frontend/src/components/user/UserPreview.vue
@@ -0,0 +1,95 @@
+<template>
+  <section class="user-preview" @click="goToDetails" v-if="user">
+    <img :src="user.img_url" />
+
+    <div class="info-panel">
+      <p>{{user.firstName}}</p>
+      <p>{{user.currCity}}</p>
+      <p>{{user.Activities}}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "TheUserPreview",
+  props: ["user"],
+  methods: {
+    goToDetails() {
+      // this.$router.push(`/toyApp/${this.toy._id}`);
+      this.$router.push(`/user/${this.user._id}`);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.user-preview {
+  /* width: 23%; */
+  object-fit: contain;
+  cursor: pointer;
+  min-height: 270px;
+  background-color: rgb(100, 173, 81);
+  border-radius: 4px;
+  flex-basis: 200px;
+  flex-grow: 1;
+  margin: 17px 7px;
+  max-width: 200px;
+  box-shadow: 0 0 2px #969696;
+  /* position: relative; */
+}
+
+.user-preview:hover {
+  box-shadow: 0 0 8px #969696;
+}
+
+p {
+  margin: 5px 0;
+}
+
+img {
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+  border-radius: 4px;
+}
+
+.info-panel {
+  padding: 10px;
+  text-align: left;
+  position: relative;
+  bottom: 50px;
+
+  /* ehud changed */
+
+  color: #333;
+  font-weight: 500;
+  font-weight: 700;
+  font-size: 21px;
+}
+
+/* .info-panel .name {
+  color: #333;
+  font-weight: 500;
+  font-weight: 700;
+  font-size: 21px;
+} */
+
+.expand {
+  animation: expand 1s;
+}
+
+@keyframes expand {
+  to {
+    /* left: 50%; */
+    transform: scale(1.5);
+    opacity: 0.5;
+  }
+}
+
+@media (max-width: 670px) {
+  .user-preview {
+    flex-basis: 45%;
+  }
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-
     <header class="home-header">
       <HomeNav></HomeNav>
       <!-- <img alt="Vue logo" src="../assets/cover.png"> -->
@@ -8,28 +7,34 @@
       <HelloWorld class="welcome" msg="Welcome to uThere" />
     </header>
 
-    <div class="city">
+    <div class="citys">
       <CityList></CityList>
-      </div>
+    </div>
+    <div class="users">
+      <UserList></UserList>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HomeNav from "@/components/HomeNav.vue"
+import HomeNav from "@/components/HomeNav.vue";
 import HelloWorld from "@/components/HelloWorld.vue";
 import CityList from "@/components/city/CityList.vue";
+import UserList from "@/components/user/UserList.vue";
 
 export default {
   name: "home",
   components: {
     HomeNav,
     CityList,
+    UserList,
     HelloWorld
   },
   created() {
     // this.$store.getters.cities.length || this.$store.dispatch("loadCities");
     this.$store.dispatch("loadCities");
+    this.$store.dispatch("loadUsers");
     console.log(this.$store.state);
   }
 };
@@ -37,7 +42,6 @@ export default {
 
 
 <style lang="scss" scoped>
-
 .home-header {
   position: relative;
   background-image: url("../assets/02-full.png");
@@ -53,5 +57,4 @@ export default {
   margin-left: 280px;
   margin-top: 40px;
 }
-
 </style>
