bc65b549535d3cb63b89a07fe36b29d6b86ade8a
Ehud Perlman <ehudperlman83@gmail.com>
2019-07-18 14:49:01 +0300

    heroku ready

---
+++ backend/public/precache-manifest.bc90c7a739c6c0ad5aa3481cfc8212f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "9f650aa0310497cd3183",
+    "url": "/js/chunk-vendors.739a7318.js"
+  },
+  {
+    "revision": "617a095e8e91c49e774c",
+    "url": "/js/app.ea4c4fa6.js"
+  },
+  {
+    "revision": "554dd3bab82d0769e787bf9e697860b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4595a89954a091a31c35b1859572bac",
+    "url": "/img/logo.e4595a89.png"
+  },
+  {
+    "revision": "8fdb36b51df820ac2e7bcf6533327f6f",
+    "url": "/img/02-full.8fdb36b5.png"
+  },
+  {
+    "revision": "617a095e8e91c49e774c",
+    "url": "/css/app.400a0e63.css"
+  }
+];
