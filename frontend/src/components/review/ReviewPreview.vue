832e09572309ffdc5e80b52863cbfd053b3d36e3
Ehud Perlman <ehudperlman83@gmail.com>
2019-07-24 16:28:22 +0300

    edited user details

--- frontend/src/components/review/ReviewPreview.vue
+++ frontend/src/components/review/ReviewPreview.vue
@@ -1,10 +1,14 @@
 <template>
   <section class="review-preview" v-if="review">
     <div>
+      <div :style="profileImage"></div>
       <h2>Title: {{review.title}}</h2>
       <h2>Comment: {{review.txt}}</h2>
-      <h2>Added By :{{review.addedBy}}</h2>
-      <h2>Rating: {{review.rating}}</h2>
+      <h2>Added By :{{fullName}}</h2>
+      <h2>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        {{review.rating}}
+      </h2>
     </div>
   </section>
 </template>
@@ -12,6 +16,21 @@
 <script>
 export default {
   name: "TheReviewPreview",
-  props: ["review"]
+  props: ["review"],
+  data() {
+    return {
+      profileImage: {
+        width: "150px",
+        height: "150px",
+        "border-radius": "50%",
+        background: `url(${this.review.addedBy.img_url}) no-repeat center center`
+      }
+    };
+  },
+  computed: {
+    fullName() {
+      return this.review.addedBy.firstName + " " + this.review.addedBy.lastName;
+    }
+  }
 };
 </script>
